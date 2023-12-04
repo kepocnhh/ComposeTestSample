8a3267859cad64122fcf21caff7326271fb329c6
Stanley Wintergreen <kepocnhh@gmail.com>
2023-12-04 16:46:45 +0300

    init android

---
+++ app/src/examine/kotlin/sample/compose/unittest/TestActivity.kt
@@ -0,0 +1,5 @@
+package sample.compose.unittest
+
+import androidx.activity.ComponentActivity
+
+internal class TestActivity : ComponentActivity()
---
+++ app/src/main/kotlin/sample/compose/unittest/MainActivity.kt
@@ -0,0 +1,17 @@
+package sample.compose.unittest
+
+import android.os.Bundle
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+
+internal class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BackHandler {
+                finish()
+            }
+        }
+    }
+}
