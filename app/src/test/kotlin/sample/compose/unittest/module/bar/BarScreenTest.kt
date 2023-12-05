a1c0a5bd5f59738f5e8952835705a11393252fca
Stanley Wintergreen <kepocnhh@gmail.com>
2023-12-05 14:36:42 +0300

    test bar

--- app/src/test/kotlin/sample/compose/unittest/module/bar/BarScreenTest.kt
+++ app/src/test/kotlin/sample/compose/unittest/module/bar/BarScreenTest.kt
@@ -7,12 +7,14 @@ import androidx.compose.ui.test.hasContentDescription
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.performClick
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import sample.compose.unittest.App
 import sample.compose.unittest.TestActivity
+import sample.compose.unittest.clearStores
 import sample.compose.unittest.module.app.mockInjection
 import sample.compose.unittest.provider.MockLocalDataProvider
 import sample.compose.unittest.setInjection
@@ -22,22 +24,9 @@ internal class BarScreenTest {
     @get:Rule
     val rule = createAndroidComposeRule<TestActivity>()
 
-    @Test(timeout = 10_000)
-    fun initialTextTest() {
-        val initialText = "foobar:bar"
-        val injection = mockInjection(
-            local = MockLocalDataProvider(bar = initialText),
-        )
-        App.setInjection(injection)
-        rule.setContent {
-            BarScreen()
-        }
-        val isText = hasContentDescription("BarScreen:text")
-        rule.waitUntil {
-            rule.onAllNodes(isText and hasText(initialText))
-                .fetchSemanticsNodes()
-                .size == 1
-        }
+    @Before
+    fun before() {
+        App.clearStores()
     }
 
     @Test(timeout = 10_000)
@@ -61,6 +50,50 @@ internal class BarScreenTest {
         rule.onNode(isButton and isClear).performClick()
         rule.waitUntil {
             rule.onAllNodes(isText and hasText(""))
+                .fetchSemanticsNodes()
+                .size == 1
+        }
+    }
+
+    @Test(timeout = 10_000)
+    fun initialTextTest() {
+        val initialText = "foobar:bar"
+        val injection = mockInjection(
+            local = MockLocalDataProvider(bar = initialText),
+        )
+        App.setInjection(injection)
+        rule.setContent {
+            BarScreen()
+        }
+        val isText = hasContentDescription("BarScreen:text")
+        rule.waitUntil {
+            rule.onAllNodes(isText and hasText(initialText))
+                .fetchSemanticsNodes()
+                .size == 1
+        }
+    }
+
+    @Test(timeout = 10_000)
+    fun setTest() {
+        val initialText = "foobar:bar"
+        val injection = mockInjection(
+            local = MockLocalDataProvider(bar = initialText),
+        )
+        App.setInjection(injection)
+        rule.setContent {
+            BarScreen()
+        }
+        val isText = hasContentDescription("BarScreen:text")
+        rule.waitUntil {
+            rule.onAllNodes(isText and hasText(initialText))
+                .fetchSemanticsNodes()
+                .size == 1
+        }
+        val isButton = SemanticsMatcher.expectValue(SemanticsProperties.Role, Role.Button)
+        val isClear = hasContentDescription("BarScreen:set:text")
+        rule.onNode(isButton and isClear).performClick()
+        rule.waitUntil {
+            rule.onAllNodes(isText and hasText("bar"))
                 .fetchSemanticsNodes()
                 .size == 1
         }
